359b67d00a067e17ad27d11dfc25d9df186fe91e
Zhenyu Yang <zhyang25@student.scad.edu>
2025-04-18 09:47:55 +0800

    MakeCode 项目的初始文件

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// 自动生成的代码。请勿编辑。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`00780004080200`;
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
